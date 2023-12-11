f1aa370aca596db89a9a3cfb7c106ec904ff2c00
kenanyusifli <kenan030ss@gmail.com>
2023-12-11 10:36:07 +0400

    Javascrip

---
+++ app.js
@@ -0,0 +1,13 @@
+        // Boyuk herif
+// var sentence = prompt("Adin Nedi?");
+// var bigword = sentence.toUpperCase();
+// console.log("", bigword);
+
+
+             // kicik herif
+// var sentence = prompt("Cumle yaz");
+// var smallword = sentence.toLowerCase();
+// console.log("", smallword);
+
+ var age = prompt ("Dogum Ilini Yaz")
+ console.log(2023 - age);
